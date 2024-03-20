5e57a8fbf6e022c4ec08f8c17194747a31c1e1f2
tbanyard <t.p.banyard@gmail.com>
2024-03-20 16:13:25 +0000

    Added JSON file reading functionality

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,14 +8,14 @@ import pandas as pd
 from catchment import models, views
 
 
-def analyse_data(data_dir):
+def analyse_data(data_source):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data = load_catchment_data(data_dir)
+    data = data_source.load_catchment_data()
     return compute_standard_deviation_by_day(data)
 
 def compute_standard_deviation_by_day(data):
@@ -26,11 +26,30 @@ def compute_standard_deviation_by_day(data):
 
     return daily_standard_deviation
 
-def load_catchment_data(data_dir):
-    """Load files from required paths"""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+class CSVDataSource:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
 
-    return data
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'rain_data_2015*.csv'))
+
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        data = map(models.read_variable_from_csv, data_file_paths)
+        
+        return data
+    
+class JSONDataSource:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+    
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'rain_data_2015*.json'))
+
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        data = map(models.read_variable_from_json, data_file_paths)
+        
+        return data
+
+    
