2d48df792af00c42ddfb8261365b4df1a24746da
jamie <jamie.mcquilkin@gmail.com>
2024-03-20 12:33:01 +0000

    better structure

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -17,28 +17,34 @@ def main(args):
     InFiles = args.infiles
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
-    
+
     if args.full_data_analysis:
-        compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        daily_standard_deviation = compute_data.analyse_data(
+            os.path.dirname(InFiles[0]))
+        graph_data = {'daily standard deviation': daily_standard_deviation}
+        views.visualize(graph_data)
 
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
-        
-        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
-        
+
+        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(
+            measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
+
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
-    
+
     parser.add_argument(
         'infiles',
         nargs='+',
         help='Input CSV(s) containing measurement data')
 
-    parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
-    
+    parser.add_argument('--full-data-analysis',
+                        action='store_true', dest='full_data_analysis')
+
     args = parser.parse_args()
-    
+
     main(args)
