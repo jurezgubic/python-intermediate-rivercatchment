254566198161f2e4cbdb318fc3e8867a0263faf5
jurezgubic <jure.zgubi@gmail.com>
2024-03-20 14:33:16 +0000

    Refactored and abstracted compute data

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -9,22 +9,31 @@ from catchment import models, views
 
 
 def analyse_data(data_dir):
-    """Calculate the standard deviation by day between datasets.
+    """Load data from CSV files in the data directory and calculate the standard deviation by day between datasets."""
+    data = load_catchment_data(data_dir)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
-    Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
-    of these means.
-    """
+    return daily_standard_deviation
+
+
+def load_catchment_data(data_dir):
+    """Load data from CSV files in the data directory."""
+
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
+    return data
 
+
+def compute_standard_deviation_by_day(data):
+    """Calculate the standard deviation by day between datasets."""
+
+    daily_std_list = map(models.daily_std, data)
     daily_standard_deviation = pd.concat(daily_std_list)
 
     return daily_standard_deviation
+
+
