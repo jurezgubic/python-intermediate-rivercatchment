083b222bbcfb56c5e1ff054930cb3736bb36b448
jurezgubic <jure.zgubi@gmail.com>
2024-03-21 10:19:16 +0000

    extra parse

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -42,7 +42,11 @@ def main(args):
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
 
-        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
+        view_data = {'daily sum': models.daily_total(measurement_data),
+                     'daily average': models.daily_mean(measurement_data),
+                     'daily max': models.daily_max(measurement_data),
+                     'daily min': models.daily_min(measurement_data)
+                     }
 
         views.visualize(view_data)
 
@@ -55,7 +59,14 @@ def create_argparse():
         nargs='+',
         help='Input CSV(s) containing measurement data')
 
-    parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
+    parser.add_argument(
+        '-m', '--measurements',
+        help='The name of the measurement data series to load',
+    )
+
+    parser.add_argument('--full-data-analysis',
+                        action='store_true',
+                        dest='full_data_analysis')
     return parser
 
 if __name__ == "__main__":
